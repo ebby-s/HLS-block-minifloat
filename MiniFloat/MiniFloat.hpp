1477ab65cf39a1732d7bec8d19adc8264aa43c36
Ebby Samson <es1219@ee-beholder0.ee.ic.ac.uk>
2023-02-26 12:30:20 +0000

    Converted MiniFloat to struct.

--- MiniFloat/MiniFloat.hpp
+++ MiniFloat/MiniFloat.hpp
@@ -4,8 +4,8 @@
 #include "ap_int.h"    // Use arbitrary precision types.
 
 
-template <int E, int M> class MiniFloat;     // MiniFloat.
-template <int N, int F> class KulischAcc;    // Fixed Point Accumulator.
+template <int E, int M> struct MiniFloat;     // MiniFloat.
+template <int N, int F> class  KulischAcc;    // Fixed Point Accumulator.
 
 
 template <int E, int M> struct MiniFloat{
@@ -39,42 +39,6 @@ template <int E, int M> struct MiniFloat{
         return out;
     }
 };
-
-// template <int E, int M> class MiniFloat{
-
-//     protected:
-//     ap_uint<1> sgn;
-//     ap_uint<E> exp;
-//     ap_uint<M> man;
-
-//     public:
-//     MiniFloat(){}
-
-//     KulischAcc<((1<<E)+M)*2, 2*(M-1)> operator *(const MiniFloat<E,M> &op);
-
-//     // Methods for verification, not used for synthesis.
-//     MiniFloat(int op){
-//         sgn = op >> (M+E);
-//         exp = op >>  M;
-//         if(M != 0)
-//             man = op;
-//     }
-
-//     operator float() const{  // Assume f32 has enough precision to convert exactly, and no bias.
-//         float out = 0;
-//         if(M != 0)
-//             out = man;
-//         out /= (1 << M);
-//         if(exp){
-//             out += 1;
-//         }else{
-//             out *= 2;
-//         }
-//         out *= (1 << exp);
-//         if(sgn) out *= -1;
-//         return out;
-//     }
-// };
 
 
 
