1eb3f56419e49e80a931cfaa39ab9c4f03605625
Ebby Samson <es1219@ee-beholder0.ee.ic.ac.uk>
2023-05-30 16:31:53 +0100

    Removed normalisation from mat mul.

--- BlockMat/BlockMul.hpp
+++ BlockMat/BlockMul.hpp
@@ -3,18 +3,11 @@
 
 #include "CountLZ.hpp"
 
-#define MUL_SAMT_II 1
-
-#ifndef MUL_NORM
-#define MUL_NORM false
-#endif
 
 template <int N, int E, int M>
 BlockFP<N,WPRD(E,M)+CLOG2(N),FPRD(E,M)> BlockMF<N,E,M>::operator *(BlockMF<N,E,M> &op){
 
     const int Weff = (WPRD(E,M)+CLOG2(N));
-
-    const bool norm_en = MUL_NORM;
 
     BlockFP<N,Weff,FPRD(E,M)> out;
 
@@ -26,10 +19,10 @@ BlockFP<N,WPRD(E,M)+CLOG2(N),FPRD(E,M)> BlockMF<N,E,M>::operator *(BlockMF<N,E,M
     //  Matrix multiplication //
     ////////////////////////////
 
-    zero_r_loop:  // Initialise output to zero.
+    bmf_zero_r_loop:  // Initialise output to zero.
     for (int i=0; i<N; i++){
         #pragma HLS UNROLL
-        zero_c_loop:
+        bmf_zero_c_loop:
         for (int j=0; j<N; j++){
             #pragma HLS UNROLL
 
@@ -37,13 +30,13 @@ BlockFP<N,WPRD(E,M)+CLOG2(N),FPRD(E,M)> BlockMF<N,E,M>::operator *(BlockMF<N,E,M
         }
     }
 
-    mm_dp_loop:  // MatMul by Systolic Array.
+    bmf_mm_dp_loop:  // MatMul by Systolic Array.
     for (int k=0; k<N; k++){
         #pragma HLS PIPELINE II=1
-        mm_r_loop:
+        bmf_mm_r_loop:
         for (int i=0; i<N; i++){
             #pragma HLS UNROLL
-            mm_c_loop:
+            bmf_mm_c_loop:
             for (int j=0; j<N; j++){
                 #pragma HLS UNROLL
 
@@ -52,49 +45,8 @@ BlockFP<N,WPRD(E,M)+CLOG2(N),FPRD(E,M)> BlockMF<N,E,M>::operator *(BlockMF<N,E,M
         }
     }
 
-    ////////////////////////////
-    // Optional normalisation //
-    ////////////////////////////
-
-    if(norm_en){
-        // Normalise data, calculate bias.
-        ap_uint<CLOG2(Weff+1)> ldd, shift_amt = (Weff-1);
-        samt_r_loop:
-        for(int i=0; i<N; i++){
-            DO_PRAGMA(HLS PIPELINE II=MUL_SAMT_II)
-            samt_c_loop:
-            for(int j=0; j<N; j++){
-                #pragma HLS UNROLL
-                #pragma HLS INLINE
-
-                ap_int<Weff> ldd_data;
-                if(out.data[i][j].acc[Weff-1]){
-                    ldd_data = ~out.data[i][j].acc;
-                }else{
-                    ldd_data =  out.data[i][j].acc;
-                }
-
-                ldd = CtLZ<Weff>(ldd_data);
-
-                if((ldd-1) < shift_amt)
-                    shift_amt = ldd-1;
-            }
-        }
-        out.bias = bias + op.bias - shift_amt;
-
-        shift_r_loop:  // Apply shift to data.
-        for(int i=0; i<N; i++){
-            #pragma HLS UNROLL
-            shift_c_loop:
-            for(int j=0; j<N; j++){
-                #pragma HLS UNROLL
-
-                out.data[i][j].acc = out.data[i][j].acc << shift_amt;
-            }
-        }
-    }else{
-        out.bias = bias + op.bias;
-    }
+    // Add biases.
+    out.bias = bias + op.bias;
 
     return out;
 }
@@ -104,8 +56,6 @@ template <int N, int W, int F>
 BlockFP<N,2*W+CLOG2(N),2*F> BlockFP<N,W,F>::operator *(BlockFP<N,W,F> &op){
 
     const int Weff = (2*W+CLOG2(N));
-
-    const bool norm_en = MUL_NORM;
 
     BlockFP<N,Weff,2*F> out;
 
@@ -117,10 +67,10 @@ BlockFP<N,2*W+CLOG2(N),2*F> BlockFP<N,W,F>::operator *(BlockFP<N,W,F> &op){
     //  Matrix multiplication //
     ////////////////////////////
 
-    zero_r_loop:  // Initialise output to zero.
+    bfp_zero_r_loop:  // Initialise output to zero.
     for (int i=0; i<N; i++){
         #pragma HLS UNROLL
-        zero_c_loop:
+        bfp_zero_c_loop:
         for (int j=0; j<N; j++){
             #pragma HLS UNROLL
 
@@ -128,13 +78,13 @@ BlockFP<N,2*W+CLOG2(N),2*F> BlockFP<N,W,F>::operator *(BlockFP<N,W,F> &op){
         }
     }
 
-    mm_dp_loop:  // MatMul by Systolic Array.
+    bfp_mm_dp_loop:  // MatMul by Systolic Array.
     for (int k=0; k<N; k++){
         #pragma HLS PIPELINE II=1
-        mm_r_loop:
+        bfp_mm_r_loop:
         for (int i=0; i<N; i++){
             #pragma HLS UNROLL
-            mm_c_loop:
+            bfp_mm_c_loop:
             for (int j=0; j<N; j++){
                 #pragma HLS UNROLL
 
@@ -143,49 +93,8 @@ BlockFP<N,2*W+CLOG2(N),2*F> BlockFP<N,W,F>::operator *(BlockFP<N,W,F> &op){
         }
     }
 
-    ////////////////////////////
-    // Optional normalisation //
-    ////////////////////////////
-
-    if(norm_en){
-        // Normalise data, calculate bias.
-        ap_uint<CLOG2(Weff+1)> ldd, shift_amt = (Weff-1);
-        samt_r_loop:
-        for(int i=0; i<N; i++){
-            DO_PRAGMA(HLS PIPELINE II=MUL_SAMT_II)
-            samt_c_loop:
-            for(int j=0; j<N; j++){
-                #pragma HLS UNROLL
-                #pragma HLS INLINE
-
-                ap_int<Weff> ldd_data;
-                if(out.data[i][j].acc[Weff-1]){
-                    ldd_data = ~out.data[i][j].acc;
-                }else{
-                    ldd_data =  out.data[i][j].acc;
-                }
-
-                ldd = CtLZ<Weff>(ldd_data);
-
-                if((ldd-1) < shift_amt)
-                    shift_amt = ldd-1;
-            }
-        }
-        out.bias = bias + op.bias - shift_amt;
-
-        shift_r_loop:  // Apply shift to data.
-        for(int i=0; i<N; i++){
-            #pragma HLS UNROLL
-            shift_c_loop:
-            for(int j=0; j<N; j++){
-                #pragma HLS UNROLL
-
-                out.data[i][j].acc = out.data[i][j].acc << shift_amt;
-            }
-        }
-    }else{
-        out.bias = bias + op.bias;
-    }
+    // Add biases.
+    out.bias = bias + op.bias;
 
     return out;
 }
