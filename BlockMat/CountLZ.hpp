24037e3600fcf653d5f647a609f5fd4937362ccf
Ebby Samson <es1219@ee-beholder0.ee.ic.ac.uk>
2023-06-06 10:22:06 +0100

    Cleaned up clz.

--- BlockMat/CountLZ.hpp
+++ BlockMat/CountLZ.hpp
@@ -4,23 +4,35 @@
 
 #include "ap_int.h"
 
+// Wrapper to choose between builtin implementations for clz.
 template <int W>
 ap_uint<CLOG2(W+1)> __CtLZ_builtin(ap_uint<W> data){
 
-    if(W <= 32){
+    if(W <= 32){            // Use 32-bit builtin CLZ.
         if(data == 0){
             return W;
         }else{
             return __builtin_clz(data) - (32-W);
         }
-    }else if(W <= 64){
+
+    }else if(W <= 64){     // Use 64-bit builtin CLZ.
         if(data == 0){
             return W;
         }else{
             return __builtin_clzl(data) - (64-W);
         }
+
+    }else if(W <= 96){
+        if(data == 0){     // Use 32-bit + 64-bit builtin CLZ.
+            return W;
+        }else if(data(W-1, 64) == 0){
+            return __builtin_clzl(data(63, 0)) + (W-64);
+        }else{
+            return __builtin_clz(data(W-1, 64)) - (96-W);
+        }
+
     }else{
-        if(data == 0){
+        if(data == 0){     // Use 2x 64-bit builtin CLZ.
             return W;
         }else if(data(W-1, 64) == 0){
             return __builtin_clzl(data(63, 0)) + (W-64);
@@ -55,7 +67,7 @@ template <int W>
 ap_uint<CLOG2(W+1)> CtLZ(ap_uint<W> data){
     #pragma HLS INLINE
 
-    if(W > 18){
+    if((W > 18) && (W <= 128)){
         return __CtLZ_builtin<W>(data);
     }else{
         return __CtLZ_custom<W>(data);
