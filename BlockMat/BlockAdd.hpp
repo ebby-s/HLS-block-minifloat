2773ba3a603558453d8f5f0fd6f5ae0846cdcec5
Ebby Samson <es1219@ee-beholder0.ee.ic.ac.uk>
2023-06-06 21:06:30 +0100

    Added ability to unroll addition loop.

--- BlockMat/BlockAdd.hpp
+++ BlockMat/BlockAdd.hpp
@@ -1,14 +1,8 @@
 #ifndef B_ADD
 #define B_ADD
 
-#define ADD_SAMT_II 1
-
 #ifndef ADD_UNROLL
-#define ADD_UNROLL false
-#endif
-
-#ifndef ADD_POST_NORM
-#define ADD_POST_NORM false
+#define ADD_UNROLL 1
 #endif
 
 template<int N, int W, int F>
@@ -16,13 +10,21 @@ BlockFP<N,W+3+1,F+3> BlockFP<N,W,F>::operator +(BlockFP<N,W,F> &op){
 
     const int Weff = (W+3+1);
 
-    const bool post_norm_en = ADD_POST_NORM;
-
     BlockFP<N,Weff,F+3> out;
 
-    #pragma HLS ARRAY_PARTITION variable=data     dim=0 complete
-    #pragma HLS ARRAY_PARTITION variable=op.data  dim=0 complete
-    #pragma HLS ARRAY_PARTITION variable=out.data dim=0 complete
+    #if (ADD_UNROLL == 0)
+        #pragma HLS ARRAY_PARTITION variable=data     dim=1 complete
+        #pragma HLS ARRAY_PARTITION variable=op.data  dim=1 complete
+        #pragma HLS ARRAY_PARTITION variable=out.data dim=1 complete
+    #elif (ADD_UNROLL != 1)
+        DO_PRAGMA(HLS ARRAY_PARTITION variable=data     dim=1 cyclic factor=ADD_UNROLL)
+        DO_PRAGMA(HLS ARRAY_PARTITION variable=op.data  dim=1 cyclic factor=ADD_UNROLL)
+        DO_PRAGMA(HLS ARRAY_PARTITION variable=out.data dim=1 cyclic factor=ADD_UNROLL)
+    #endif
+
+    #pragma HLS ARRAY_PARTITION variable=data     dim=2 complete
+    #pragma HLS ARRAY_PARTITION variable=op.data  dim=2 complete
+    #pragma HLS ARRAY_PARTITION variable=out.data dim=2 complete
 
     ////////////////////////////
     //         Addition Loops //
@@ -35,11 +37,15 @@ BlockFP<N,W+3+1,F+3> BlockFP<N,W,F>::operator +(BlockFP<N,W,F> &op){
     // Shift and add data.
     add_r_loop:
     for (int i=0; i<N; i++){
-        #if ADD_UNROLL
+
+        #if (ADD_UNROLL == 0)
             #pragma HLS UNROLL
+        #elif (ADD_UNROLL == 1)
+            #pragma HLS PIPELINE II=1
         #else
-            DO_PRAGMA(HLS PIPELINE II=ADD_SAMT_II)
+            DO_PRAGMA(HLS UNROLL factor=ADD_UNROLL)
         #endif
+
         add_c_loop:
         for (int j=0; j<N; j++){
             #pragma HLS UNROLL
@@ -69,49 +75,8 @@ BlockFP<N,W+3+1,F+3> BlockFP<N,W,F>::operator +(BlockFP<N,W,F> &op){
         }
     }
 
-    ////////////////////////////
-    // Optional Normalisation //
-    ////////////////////////////
-
-    if(post_norm_en){
-        // Normalise data, calculate bias.
-        ap_uint<CLOG2(Weff+1)> ldd, shift_amt = (Weff-1);
-        samt_r_loop:
-        for(int i=0; i<N; i++){
-            DO_PRAGMA(HLS PIPELINE II=MUL_SAMT_II)
-            samt_c_loop:
-            for(int j=0; j<N; j++){
-                #pragma HLS UNROLL
-                #pragma HLS INLINE
-
-                ap_int<Weff> ldd_data;
-                if(out.data[i][j].acc[Weff-1]){
-                    ldd_data = ~out.data[i][j].acc;
-                }else{
-                    ldd_data =  out.data[i][j].acc;
-                }
-
-                ldd = CtLZ<Weff>(ldd_data);
-
-                if((ldd-1) < shift_amt)
-                    shift_amt = ldd-1;
-            }
-        }
-        out.bias = (op0_lrg ? bias : op.bias) - shift_amt;
-
-        shift_r_loop:  // Apply shift to data.
-        for(int i=0; i<N; i++){
-            #pragma HLS UNROLL
-            shift_c_loop:
-            for(int j=0; j<N; j++){
-                #pragma HLS UNROLL
-
-                out.data[i][j].acc = out.data[i][j].acc << shift_amt;
-            }
-        }
-    }else{
-        out.bias = op0_lrg ? bias : op.bias;
-    }
+    // Larger bias is output bias.
+    out.bias = op0_lrg ? bias : op.bias;
 
     return out;
 }
