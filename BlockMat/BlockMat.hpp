e6a4bd78f64b43c9c0b64abbf64e2774ff88eaa0
Ebby Samson <es1219@ee-beholder0.ee.ic.ac.uk>
2023-06-06 09:00:40 +0100

    Unrolled copy constructor and assignment.

--- BlockMat/BlockMat.hpp
+++ BlockMat/BlockMat.hpp
@@ -16,6 +16,46 @@ template<int N, int E, int M> struct BlockMF{
     BlockFP<N,WPRD(E,M)+CLOG2(N),FPRD(E,M)> operator *(BlockMF<N,E,M> &op);  // Multiply a pair of BMF blocks.
 
     operator BlockFP<N,WPRD(E,M)/2,FPRD(E,M)/2>() const; // Convert a BMF block to BFP.
+
+    BlockMF<N,E,M>(){}
+
+    BlockMF<N,E,M>(const BlockMF<N,E,M> &op){
+
+        bias = op.bias;
+
+        bmf_copy_r_loop:
+        for(int i=0; i<N; i++){
+            #pragma HLS UNROLL
+            bmf_copy_c_loop:
+            for(int j=0; j<N; j++){
+                #pragma HLS UNROLL
+
+                data[i][j] = op.data[i][j];
+            }
+        }
+    }
+
+    BlockMF<N,E,M> &operator=(const BlockMF<N,E,M>& rhs){
+
+        if(&rhs == this)
+            return *this;
+
+        bias = rhs.bias;
+
+        bmf_assign_r_loop:
+        for(int i=0; i<N; i++){
+            #pragma HLS UNROLL
+            bmf_assign_c_loop:
+            for(int j=0; j<N; j++){
+                #pragma HLS UNROLL
+
+                data[i][j] = rhs.data[i][j];
+            }
+        }
+
+        return *this; 
+    }
+
 };
 
 
@@ -35,6 +75,45 @@ template<int N, int W, int F> struct BlockFP{
 
     template<int Wo, int Fo>
     operator BlockFP<N,Wo,Fo>() const;      // Round down a BFP block and change frac bits.
+
+    BlockFP<N,W,F>(){}
+
+    BlockFP<N,W,F>(const BlockFP<N,W,F> &op){
+
+        bias = op.bias;
+
+        bfp_copy_r_loop:
+        for(int i=0; i<N; i++){
+            #pragma HLS UNROLL
+            bfp_copy_c_loop:
+            for(int j=0; j<N; j++){
+                #pragma HLS UNROLL
+
+                data[i][j] = op.data[i][j];
+            }
+        }
+    }
+
+    BlockFP<N,W,F> &operator=(const BlockFP<N,W,F>& rhs){
+
+        if(&rhs == this)
+            return *this;
+
+        bias = rhs.bias;
+
+        bfp_assign_r_loop:
+        for(int i=0; i<N; i++){
+            #pragma HLS UNROLL
+            bfp_assign_c_loop:
+            for(int j=0; j<N; j++){
+                #pragma HLS UNROLL
+
+                data[i][j] = rhs.data[i][j];
+            }
+        }
+
+        return *this; 
+    }
 };
 
 
